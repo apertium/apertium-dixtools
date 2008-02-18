c3399928acffaeec8a9256e44410a34148a50cfd
Enrique Benimeli Bofarull <dev@ebenimeli.org>
2008-02-18 17:03:27 +0000

    last commit today
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-crossdics@3829 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ src/misc/enes/CompleteTranslation.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2007 Universitat d'Alacant / Universidad de Alicante
+ * Author: Enrique Benimeli Bofarull
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+package misc.enes;
+
+import dics.elements.dtd.DictionaryElement;
+import dics.elements.dtd.EElement;
+import dics.elements.dtd.IElement;
+import dics.elements.dtd.LElement;
+import dics.elements.dtd.PElement;
+import dics.elements.dtd.RElement;
+import dics.elements.dtd.SElement;
+import dics.elements.dtd.TextElement;
+import dics.elements.utils.SElementList;
+import dictools.DictionaryReader;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Enrique Benimeli Bofarull
+ */
+public class CompleteTranslation {
+
+   /**
+    * 
+    */
+   private DictionaryElement bil;
+   /**
+    * 
+    */
+   private HashMap<String, String> translations;
+
+   /**
+    * 
+    * @param dicFileName
+    * @param sourceFileName
+    * @param translationFileName
+    */
+   public CompleteTranslation(final String dicFileName, final String sourceFileName, final String translationFileName) {
+      translations = this.readTranslations(sourceFileName, translationFileName);
+      DictionaryReader dicReader = new DictionaryReader(dicFileName);
+      bil = dicReader.readDic();
+   }
+
+   /**
+    * 
+    */
+   public final void complete() {
+      for (EElement ee : bil.getAllEntries()) {
+         LElement left = ee.getLeft();
+         if (left != null) {
+            System.out.println("Completing " + left.getValueNoTags());
+            SElementList slist = left.getSElements();
+            String source = left.getValueNoTags();
+            if (source != null) {
+               String translation = translations.get(source);
+               if (translation != null) {
+                  RElement rE = new RElement();
+                  TextElement tE = new TextElement(translation);
+                  rE.addChild(tE);
+                  for (SElement sE : slist) {
+                     rE.addChild(sE);
+                  }
+                  if (ee.getP() != null) {
+                  ee.getP().setRElement(rE);
+                  } else {
+                     PElement pE = new PElement();
+                     pE.setRElement(rE);
+                     pE.setRElement(ee.getRight());
+                     IElement iE = ee.getI();
+                     ee.getChildren().remove(iE);
+                     ee.addChild(pE);                     
+                  }
+               }
+            }
+         }
+
+      }
+      bil.printXML("trans-completed-2.dix");
+   }
+
+   /**
+    * 
+    * @param sfn
+    * @param tfn
+    * @return
+    */
+   private final HashMap<String, String> readTranslations(final String sfn, final String tfn) {
+      HashMap<String, String> map = new HashMap<String, String>();
+      ArrayList<String> srcList = this.buildList(sfn);
+      ArrayList<String> transList = this.buildList(tfn);
+
+      for (int i = 0; i < srcList.size(); i++) {
+         String src = srcList.get(i);
+         String trans = transList.get(i);
+         map.put(src, trans);
+         System.out.println(src + " --> " + trans);
+      }
+      return map;
+   }
+
+   /**
+    * 
+    * @param fileName
+    * @return
+    */
+   private final ArrayList<String> buildList(final String fileName) {
+      ArrayList<String> list = new ArrayList<String>();
+      try {
+         FileInputStream fstream = new FileInputStream(fileName);
+         DataInputStream in = new DataInputStream(fstream);
+         BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+         StreamTokenizer st = new StreamTokenizer(br);
+         st.quoteChar('.');
+
+         String strLine;
+
+         while ((strLine = br.readLine()) != null) {
+            if (!strLine.equals("")) {
+               //System.err.println("'" + strLine + "'");
+               list.add(strLine);
+            }
+
+         }
+         in.close();
+      } catch (Exception e) {
+         System.err.println("Error: " + e.getMessage());
+      }
+      return list;
+
+   }
+}
---
+++ src/misc/enes/PrepareDic.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2007 Universitat d'Alacant / Universidad de Alicante
+ * Author: Enrique Benimeli Bofarull
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+package misc.enes;
+
+import dics.elements.dtd.DictionaryElement;
+import dics.elements.dtd.EElement;
+import dics.elements.dtd.SectionElement;
+import dictools.DictionaryReader;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Enrique Benimeli Bofarull
+ */
+public class PrepareDic {
+
+   private DictionaryElement ncBil;
+   private HashMap<String, String> missing;
+
+   /**
+    * 
+    */
+   public PrepareDic(final String missingFileName, final String ncBilFileName) {
+      DictionaryReader r1 = new DictionaryReader(ncBilFileName);
+      ncBil = r1.readDic();
+      missing = this.getmap(missingFileName);
+      this.prepare();
+   }
+
+   /**
+    * 
+    */
+   public final void prepare() {
+      DictionaryElement dic = new DictionaryElement();
+      SectionElement section = new SectionElement("main", "standard");
+      dic.addSection(section);
+
+      int max = ncBil.getAllEntries().size();
+      for (int i = 0; i < max; i++) {
+         EElement ee = ncBil.getAllEntries().get(i);
+         //String value = ee.getLeft().getValueNoTags();
+         String value = ee.getRight().getValueNoTags();
+         System.out.println("checking " + value + " ...");
+         if (value != null) {
+            if (missing.containsKey(value)) {
+               section.addEElement(ee);
+               System.out.println("adding " + value + " ...");
+            }
+         }
+      }
+      dic.printXML("not-common-en-ca-filtered.dix");
+   }
+
+   /**
+    * 
+    * @param fileName
+    * @return Map with missing entries
+    */
+   private HashMap<String, String> getmap(final String fileName) {
+      HashMap<String, String> map = new HashMap<String, String>();
+      try {
+         FileInputStream fstream = new FileInputStream(fileName);
+         DataInputStream in = new DataInputStream(fstream);
+         BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+         String strLine;
+
+         while ((strLine = br.readLine()) != null) {
+            if (!strLine.equals("")) {
+               map.put(strLine, strLine);
+               //System.err.println("'" + strLine + "'");
+            }
+         }
+         in.close();
+      } catch (Exception e) {
+         System.err.println("Error: " + e.getMessage());
+      }
+      return map;
+   }
+}
