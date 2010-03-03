6032d68899693eda171f9c48d6308f9f81792f1a
Jim O'Regan <joregan@gmail.com>
2010-03-03 17:23:37 +0000

    you need libtooliza, otherwise you get errors about ltmain.sh being missing
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@20496 72bbbca6-d526-0410-a7d9-f06f51895060

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# echo "- libtoolize."            && \
-# if test x$(uname -s) = xDarwin; then glibtoolize --force; else libtoolize --force; fi && \
+echo "- libtoolize."            && \
+if test x$(uname -s) = xDarwin; then glibtoolize --force; else libtoolize --force; fi && \
 echo "- aclocal."		&& \
 aclocal	-I m4			&& \
 echo "- autoconf."		&& \
