074559ec41be495ce5ee44ed74476c39455d4fc7
Jim O'Regan <joregan@gmail.com>
2010-03-03 17:30:50 +0000

    gcj is much more strict about missing imports
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@20498 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/dictools/utils/XMLReader.java
+++ src/dictools/utils/XMLReader.java
@@ -24,6 +24,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
