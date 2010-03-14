227a425e1963ced8be0cf87b40ea7377c794cd03
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2010-03-14 08:44:39 +0000

    <jacobEo> it should be:  <r>that's<b/>why<s n="cnjadv"/></r> <jacobEo> not  <r>that&apos;s<b/>why<s n="cnjadv"/></r>
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@20797 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/dics/elements/dtd/TextElement.java
+++ src/dics/elements/dtd/TextElement.java
@@ -55,7 +55,8 @@ public class TextElement extends DixElement {
         // so we can't escape > and < !
         // replaceAll("<", "&lt;").replaceAll(">", "&gt;").
 
-        dos.append(text.replace("&", "&amp;").replace("\"", "&quot;").replace("\'", "&apos;"));
+      // don't .replace("\"", "&quot;")   - we want entries like  <r>that's<b/>why , not <r>that&apos;s<b/>why
+        dos.append(text.replace("&", "&amp;").replace("\'", "&apos;"));
 
         //not necesary: dos.append(appendCharacterData);
     }
