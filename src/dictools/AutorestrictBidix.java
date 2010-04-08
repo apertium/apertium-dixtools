971fe5f8bd09409d9514b870fd16ce08b85e550b
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2010-04-08 10:56:32 +0000

    Fix Jimmys bug and comment out Jimmy's change temporarily
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@21632 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/dictools/AutorestrictBidix.java
+++ src/dictools/AutorestrictBidix.java
@@ -218,11 +218,12 @@ public class AutorestrictBidix extends AbstractDictTool {
 
   public static boolean isAllowed(String direction, E ee) {
     if ("yes".equals(ee.ignore)) return false;
-    if("LR".equals(direction) && ee.isAnyLR()) return false;
-    if("RL".equals(direction) && ee.isAnyRL()) return false;
+    //Comment out Jimmy's change temporarily
+    //if("LR".equals(direction) && ee.isAnyLR()) return false;
+    //if("RL".equals(direction) && ee.isAnyRL()) return false;
     String restric = ee.restriction;
     if (restric==null) return true;
-    if (restric.equals(restric)) return true;
+    if (restric.equals(direction)) return true;
     return false;    
   }
 
