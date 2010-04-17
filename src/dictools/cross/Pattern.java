1b91ac5d2cda034a8e0a3cd337321b2712b7085d
Jim O'Regan <joregan@gmail.com>
2010-04-17 00:58:32 +0000

    find/replace in licence text
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@21871 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/dictools/cross/Pattern.java
+++ src/dictools/cross/Pattern.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2007 Universitat d'Alacant / Universidad de Alicante
  * Author: Enrique Benimeli Bofarull
  * 
- * This program isFirstSymbol free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
