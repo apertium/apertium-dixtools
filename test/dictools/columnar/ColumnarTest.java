8506b67d94d62ffbea5014e09048a27d66027736
Jim O'Regan <joregan@gmail.com>
2011-04-17 20:25:59 +0000

    stub
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@29606 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ test/dictools/columnar/ColumnarTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011 European Commission
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
+package dictools.columnar;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jimregan
+ */
+public class ColumnarTest {
+
+    public ColumnarTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of doColumnar method, of class Columnar.
+     */
+    @Test
+    public void testDoColumnar() {
+        System.out.println("doColumnar");
+        Columnar instance = new Columnar();
+        instance.doColumnar();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
