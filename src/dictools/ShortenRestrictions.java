bd3ebbcef6b19a4a14a1a31f991f4059cf933cdf
Víctor Manuel Sánchez Cartagena <vitaka@gmail.com>
2012-09-26 13:46:52 +0000

    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-dixtools@41301 72bbbca6-d526-0410-a7d9-f06f51895060

--- src/dictools/ShortenRestrictions.java
+++ src/dictools/ShortenRestrictions.java
@@ -26,16 +26,30 @@ import java.util.List;
 public class ShortenRestrictions extends AbstractDictTool{
 
      private Dictionary dicOrig;
-    
+     private String reverseFileName;
+     private boolean rightSide=false;
     
     public ShortenRestrictions() {
     }
     
      private void processArguments() {
-        DictionaryReader dicReader = new DictionaryReader(arguments[1]);
+        reverseFileName = "short-dic.dix";
+        
+        int posArgument=1;
+        if (arguments[posArgument].equals("-right"))
+        {
+            rightSide=true;
+            posArgument++;
+        }
+        
+        DictionaryReader dicReader = new DictionaryReader(arguments[posArgument]);
         Dictionary bil = dicReader.readDic();
         dicReader = null;
         setDicOrig(bil);
+        posArgument++;
+        
+        if (arguments.length > posArgument)
+                reverseFileName = arguments[posArgument];
     }
      
        private void setDicOrig(Dictionary dicOrig) {
@@ -57,7 +71,7 @@ public class ShortenRestrictions extends AbstractDictTool{
             Iterator<E> elementIterator=section.elements.iterator();
             while(elementIterator.hasNext()) {
                 E e=elementIterator.next();
-                if (!e.containsRegEx() && e.is_LR_or_LRRL() && ("cat".equals(e.v) || "".equals(e.v) || null==e.v ) && ("std".equals(e.alt) || "".equals(e.alt) || null==e.alt )) {
+                if (!e.containsRegEx() && ( (e.is_LR_or_LRRL() && !rightSide) || (e.is_RL_or_LRRL() && rightSide) ) && ("cat".equals(e.v) || "".equals(e.v) || null==e.v ) && ("std".equals(e.alt) || "".equals(e.alt) || null==e.alt )) {
                        String lLemma=e.getLemmaWithBGForSide("L");
                        String rLemma=e.getLemmaWithBGForSide("R");
                        List<S> ltags=e.getSymbols("L");
@@ -66,12 +80,14 @@ public class ShortenRestrictions extends AbstractDictTool{
                        //ignore entries with no tags
                        if(ltags.size()>0 && rtags.size()>0)
                        {
-                           if (!lLemma.equals(prevLemma) || !ltags.get(0).getValue().equals(prevPos))
+                           String lemma=(rightSide?rLemma:lLemma);
+                           List<S> tags=(rightSide?rtags:ltags);
+                           if (!lemma.equals(prevLemma) || !tags.get(0).getValue().equals(prevPos))
                            {
                                elementsToAdd.addAll(reduceGroup(prevElements));
                                prevElements.clear();
-                               prevLemma=lLemma;
-                               prevPos=ltags.get(0).getValue();
+                               prevLemma=lemma;
+                               prevPos=tags.get(0).getValue();
                            }
                            prevElements.add(e);
                            elementIterator.remove();
@@ -86,10 +102,7 @@ public class ShortenRestrictions extends AbstractDictTool{
             }
         }
         
-        String reverseFileName = "short-dic.dix";
-        if (arguments.length == 3)
-                reverseFileName = arguments[2];
-            
+                  
         this.dicOrig.printXMLToFile(reverseFileName,opt);
         
     }
@@ -113,6 +126,18 @@ public class ShortenRestrictions extends AbstractDictTool{
            rTags.add(rtags);
         }
         
+        List<List<S>> thisSideTags=lTags;
+        List<String> thisSideLemmas=lLemmas;
+        List<List<S>> otherSideTags=rTags;
+        List<String> otherSideLemmas=rLemmas;
+        if(rightSide)
+        {
+            thisSideTags=rTags;
+            otherSideTags=lTags;
+            thisSideLemmas=rLemmas;
+            otherSideLemmas=lLemmas;
+        }
+        
         int numElements=prevElements.size();
         
         boolean allCanBeRemoved=true;
@@ -123,8 +148,8 @@ public class ShortenRestrictions extends AbstractDictTool{
             int longestEntryLength=0;
             for(int i=0; i<numElements; i++)
             {
-                if(lTags.get(i).size()>longestEntryLength)
-                    longestEntryLength=lTags.get(i).size();
+                if(thisSideTags.get(i).size()>longestEntryLength)
+                    longestEntryLength=thisSideTags.get(i).size();
             }
 
             
@@ -135,9 +160,9 @@ public class ShortenRestrictions extends AbstractDictTool{
                 allCanBeRemoved=true;
                 for(int i=0; i<numElements; i++)
                 {
-                    if(lTags.get(i).size()==longestEntryLength)
+                    if(thisSideTags.get(i).size()==longestEntryLength)
                     {
-                        if(!lTags.get(i).get(longestEntryLength-1).getValue().equals( rTags.get(i).get( rTags.get(i).size() -1 ).getValue() ))
+                        if(!thisSideTags.get(i).get(longestEntryLength-1).getValue().equals( otherSideTags.get(i).get( otherSideTags.get(i).size() -1 ).getValue() ))
                         {
                             allCanBeRemoved=false;
                             break;
@@ -151,33 +176,33 @@ public class ShortenRestrictions extends AbstractDictTool{
             {
                 //Check if, after removing the last tag from the longest entry, there are
                 //entries with the same left side and different right side
-                List<List<S>> lTagsAfterRlastTag=new ArrayList<List<S>>();
-                List<List<S>> rTagsAfterRlastTag=new ArrayList<List<S>>();
+                List<List<S>> thisSideTagsAfterRlastTag=new ArrayList<List<S>>();
+                List<List<S>> otherSideTagsAfterRlastTag=new ArrayList<List<S>>();
                 for(int i=0; i<numElements; i++)
                 {
-                    if(lTags.get(i).size()==longestEntryLength)
+                    if(thisSideTags.get(i).size()==longestEntryLength)
                     {
-                        List<S> leftSide=lTags.get(i).subList(0, longestEntryLength-1);
-                        List<S> rightSide=rTags.get(i).subList(0, rTags.get(i).size()-1);
+                        List<S> thisSide=thisSideTags.get(i).subList(0, longestEntryLength-1);
+                        List<S> otherSide=otherSideTags.get(i).subList(0, otherSideTags.get(i).size()-1);
                         
-                        lTagsAfterRlastTag.add(leftSide);
-                        rTagsAfterRlastTag.add(rightSide);
+                        thisSideTagsAfterRlastTag.add(thisSide);
+                        otherSideTagsAfterRlastTag.add(otherSide);
                     }
                     else
                     {
-                        lTagsAfterRlastTag.add(lTags.get(i));
-                        rTagsAfterRlastTag.add(rTags.get(i));
+                        thisSideTagsAfterRlastTag.add(thisSideTags.get(i));
+                        otherSideTagsAfterRlastTag.add(otherSideTags.get(i));
                     }
                 }
                 
                 for(int i=0; i<numElements; i++)
                 {
-                    List<S> curLeftSide=lTagsAfterRlastTag.get(i);
+                    List<S> curThisSide=thisSideTagsAfterRlastTag.get(i);
                     for(int j=0; j<i; j++)
                     {
-                        if(lTagsAfterRlastTag.get(j).equals(curLeftSide))
+                        if(thisSideTagsAfterRlastTag.get(j).equals(curThisSide))
                         {
-                            if(!rLemmas.get(i).equals(rLemmas.get(j)) || !rTagsAfterRlastTag.get(i).equals(rTagsAfterRlastTag.get(j)))
+                            if(!otherSideLemmas.get(i).equals(otherSideLemmas.get(j)) || !otherSideTagsAfterRlastTag.get(i).equals(otherSideTagsAfterRlastTag.get(j)))
                             {
                                 allCanBeRemoved=false;
                                 break;
@@ -193,15 +218,16 @@ public class ShortenRestrictions extends AbstractDictTool{
                 {
                     for(int i=0; i<numElements; i++)
                     {
-                        if(lTags.get(i).size()==longestEntryLength)
+                        if(thisSideTags.get(i).size()==longestEntryLength)
                         {
-                            lTags.get(i).remove(longestEntryLength-1);
-                            rTags.get(i).remove(rTags.get(i).size()-1);
+                            thisSideTags.get(i).remove(longestEntryLength-1);
+                            otherSideTags.get(i).remove(otherSideTags.get(i).size()-1);
                         }
                     }
                 }
             }
         }
+        
         
         //Remove duplicated entries
          for(int i=0; i<numElements; i++)
@@ -209,7 +235,7 @@ public class ShortenRestrictions extends AbstractDictTool{
              boolean isDuplicated=false;
              for(int  j=0; j< i; j++)
              {
-                 if(lTags.get(i).equals(lTags.get(j)) && rLemmas.get(i).equals(rLemmas.get(j)) && rTags.get(i).equals(rTags.get(j)))
+                 if(thisSideTags.get(i).equals(thisSideTags.get(j)) && otherSideLemmas.get(i).equals(otherSideLemmas.get(j)) && otherSideTags.get(i).equals(otherSideTags.get(j)))
                      isDuplicated=true;
              }
              
@@ -220,6 +246,7 @@ public class ShortenRestrictions extends AbstractDictTool{
                  L l = new L();
                  l.children.add(new TextElement(lLemmas.get(i)));
                  l.children.addAll(lTags.get(i));
+                 
                  R r = new R();
                  r.children.add(new TextElement(rLemmas.get(i)));
                  r.children.addAll(rTags.get(i));
